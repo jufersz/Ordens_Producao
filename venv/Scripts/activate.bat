275c857ef380657f3adc5be76e89c8cb3b126dc9
JULIA ALDROVANDI FERREIRA <julia_ferreira151@estudante.sesisenai.org.br>
2026-04-01 11:25:05 -0300

    Atualização de Commit

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\julia_ferreira151\Downloads\Ordens_Producao\venv
+set VIRTUAL_ENV=C:\Users\julia_ferreira151\Documents\Ordens_Producao\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
